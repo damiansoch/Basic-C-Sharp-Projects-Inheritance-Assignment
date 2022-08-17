d2fa9e74a09549b7f5866f426ccd484c5d6619af
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-17 09:08:41 +0100

    Add project files.

---
+++ Inheritance Assignment/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_Assignment
+{
+    public class Person
+    {
+        //properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //methods
+        public void SayName()
+        {
+            Console.WriteLine("Name:[{0} {1}]", FirstName, LastName);
+        }
+    }
+}
---
+++ Inheritance Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Inheritance_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee_1 = new Employee();
+
+            employee_1.FirstName = "Sample";
+            employee_1.LastName = "Student";
+
+            employee_1.SayName();
+        }
+    }
+}
